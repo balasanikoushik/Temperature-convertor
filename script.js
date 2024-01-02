172b5e270e005b48dd1d5d73919754cdefee25a6
balasanikoushik <koushikbalasani187@gmail.com>
2024-01-02 13:28:31 +0530

    upgraded background

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ let celsius = document.getElementById('celsius');
             function celCheck(val){
                 val = parseInt(val);
                 if(val > 30){
+                    document.body.style.backgroundImage = "url('./images/img-4.webp')";
                     document.getElementById("det").innerHTML="&#129397;";
                 }
                 else if(val>0){
                     if(val<15){
+                        document.body.style.backgroundImage = "url('./images/img-2.webp')";
                         document.getElementById("det").innerHTML="&#128556";
                     }
                     else{
+                        document.body.style.backgroundImage = "url('./images/img-3.webp')";
                         document.getElementById("det").innerHTML="&#128512";
                     }
                 }           
                 if(val<0){
+                    document.body.style.backgroundImage = "url('./images/img-1.webp')";
                     document.getElementById("det").innerHTML="&#129398;";
+
                 }
 
             }
